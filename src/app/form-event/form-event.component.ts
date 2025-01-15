965d3b2f443e3c7e98f95f469adc33822a83bebf
MATEAM\dvirmo <dvirmo@matrix.co.il>
2025-01-15 16:13:24 +0200

    add from-event code cases

--- src/app/form-event/form-event.component.ts
+++ src/app/form-event/form-event.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { fromEvent, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-form-event',
   imports: [],
-  templateUrl: './form-event.component.html',
+  template: `
+  <div id="target">Click me (RxJS fromEvent)</div>
+  <p>Console will log CD activity</p>
+  <!-- <button>Subscribe with Async</button> -->
+  @if(subWithAsync()) {}
+  `,
   styleUrl: './form-event.component.css'
 })
 export class FormEventComponent {
 
+  subWithAsync = signal(false);
+
+  kill = new Subject<void>();
+
+  constructor() {
+    const event = fromEvent(window, 'scroll');
+
+    event.pipe(takeUntil(this.kill))
+      .subscribe(() => console.log("scrolling"));
+
+    effect(() => {
+      if (this.subWithAsync()) {
+        this.kill.next();
+      }
+    })
+  }
+
+
+
+  ngDoCheck() {
+    console.log("FormEventComponent called")
+  }
 }
