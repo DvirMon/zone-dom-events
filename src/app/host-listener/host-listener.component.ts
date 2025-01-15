5fd1606ce8c45617d55d4fa5b4c09b2ffb468d9b
MATEAM\dvirmo <dvirmo@matrix.co.il>
2025-01-15 14:24:22 +0200

    feat - create comps and remove extra style and html

---
+++ src/app/host-listener/host-listener.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host-listener',
+  imports: [],
+  templateUrl: './host-listener.component.html',
+  styleUrl: './host-listener.component.css'
+})
+export class HostListenerComponent {
+
+}
---
+++ src/app/native-binding/native-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-native-binding',
+  imports: [],
+  templateUrl: './native-binding.component.html',
+  styleUrl: './native-binding.component.css'
+})
+export class NativeBindingComponent {
+
+}
