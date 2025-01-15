4c6be7138cfaf57ef20cac9c7947d6eddf7366b8
MATEAM\dvirmo <dvirmo@matrix.co.il>
2025-01-15 16:12:48 +0200

    feat - add native-binding code cases

--- src/app/native-binding/native-binding.component.ts
+++ src/app/native-binding/native-binding.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, effect, ElementRef, inject, NgZone, viewChild, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-native-binding',
   imports: [],
-  templateUrl: './native-binding.component.html',
+  template: `
+  <button #buttonZone>Click with zone</button>
+  <button #buttonZoneless>Click outside zone</button>
+  <p>Console will log if CD is triggered or not</p>
+  `,
   styleUrl: './native-binding.component.css'
 })
 export class NativeBindingComponent {
 
+  buttonZoneRef = viewChild<ElementRef<HTMLButtonElement>>('buttonZone')
+
+  buttonZonelessRef = viewChild<ElementRef<HTMLButtonElement>>('buttonZoneless')
+
+  ngZone = inject(NgZone);
+
+  buttonEffect = effect(() => {
+
+    const buttonZoneElem = this.buttonZoneRef();
+    if (buttonZoneElem) {
+
+      buttonZoneElem.nativeElement.addEventListener('click', () => {
+        console.log('Native event binding triggered (CD enabled)');
+      });
+
+
+    }
+
+    const buttonZonelessElm = this.buttonZonelessRef();
+
+    if (buttonZonelessElm) {
+      this.ngZone.runOutsideAngular(() => {
+        buttonZonelessElm.nativeElement.addEventListener('click', () => {
+          console.log('Native event binding triggered (CD disabled)');
+        });
+      });
+    }
+  })
+
+  ngDoCheck() {
+    console.log("NativeBindingComponent called")
+  }
 }
